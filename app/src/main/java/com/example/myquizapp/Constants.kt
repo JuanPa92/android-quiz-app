6b37fd80c3ca28e6f76668bd171f111eca3b5507
Juan Pablo Saucedo Gonzalez <juan.p.s.gonzalez.92@gmail.com>
2022-09-27 13:57:02 -0500

    Setting up the UI for the new activity Question

---
+++ app/src/main/java/com/example/myquizapp/Constants.kt
@@ -0,0 +1,71 @@
+package com.example.myquizapp
+
+object Constants {
+
+    fun getQuestions(): ArrayList<Question> {
+        val questionList = ArrayList<Question>()
+
+        val question1 = Question(
+            1,
+            "What country does this flag belongs to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Mexico",
+            "Bulgaria",
+            "Turkic",
+            1
+        )
+
+        val question2 = Question(
+            2,
+            "What country does this flag belongs to?",
+            R.drawable.ic_flag_of_belgium,
+            "Argentina",
+            "Mexico",
+            "Belgium",
+            "Turkic",
+            3
+        )
+
+        val question3 = Question(
+            3,
+            "What country does this flag belongs to?",
+            R.drawable.ic_flag_of_brazil,
+            "Brazil",
+            "Mexico",
+            "Bulgaria",
+            "Turkia",
+            1
+        )
+
+        val question4 = Question(
+            4,
+            "What country does this flag belongs to?",
+            R.drawable.ic_flag_of_denmark,
+            "Argentina",
+            "Mexico",
+            "Bulgaria",
+            "Denmark",
+            4
+        )
+
+        val question5 = Question(
+            5,
+            "What country does this flag belongs to?",
+            R.drawable.ic_flag_of_fiji,
+            "Argentina",
+            "Mexico",
+            "Fiji",
+            "Denmark",
+            3
+        )
+
+        questionList.add(question1)
+        questionList.add(question2)
+        questionList.add(question3)
+        questionList.add(question4)
+        questionList.add(question5)
+
+        return questionList
+    }
+}
